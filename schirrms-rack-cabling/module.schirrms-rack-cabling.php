295b5dd401af12661b61db4bcedd3eaca9a72dab
Schirrms <schirrms@schirrms.net>
2019-10-18 19:34:46 +0200

    Again... (0.1.0.5)

--- schirrms-rack-cabling/module.schirrms-rack-cabling.php
+++ schirrms-rack-cabling/module.schirrms-rack-cabling.php
@@ -16,7 +16,7 @@ SetupWebPage::AddModule(
 		//
 		'dependencies' => array(
 			'itop-config-mgmt/2.5.0',
-			'molkobain-datacenter-view/2.4.0',
+			'molkobain-datacenter-view/1.5.0',
 			'schirrms-comm-interface/0.6.6'
 		),
 		'mandatory' => false,
