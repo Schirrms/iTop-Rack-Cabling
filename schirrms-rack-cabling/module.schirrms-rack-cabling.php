57e02580f31b7e4ec01dcef6e1fabf9a1930d7f8
Schirrms <schirrms@schirrms.net>
2019-10-18 19:20:26 +0200

    New attempt (0.1.0.1)

--- schirrms-rack-cabling/module.schirrms-rack-cabling.php
+++ schirrms-rack-cabling/module.schirrms-rack-cabling.php
@@ -9,14 +9,14 @@ SetupWebPage::AddModule(
 	array(
 		// Identification
 		//
-		'label' => 'Rack to rack and cable management',
+		'label' => 'Rack to rack Patch Panel and cable management',
 		'category' => 'business',
 
 		// Setup
 		//
 		'dependencies' => array(
 			'itop-config-mgmt/2.5.0',
-			'molkobain-datacenter-view/2.4.0',
+			'molkobain-datacenter-view/2.0.0',
 			'schirrms-comm-interface/0.6.6'
 		),
 		'mandatory' => false,
