ec18e3b908ef13d982a6cd0a8708e57cff0bd4dc
Schirrms <schirrms@schirrms.net>
2019-12-13 17:42:28 +0100

    Separate Class for Patch Cable Color, first text file (0.2.1.0)

--- schirrms-rack-cabling/en.dict.schirrms-rack-cabling.php
+++ schirrms-rack-cabling/en.dict.schirrms-rack-cabling.php
@@ -2,11 +2,34 @@
 /**
  * Localized data
  *
- * @copyright   Copyright (C) 2013 XXXXX
+ * @copyright   Copyright (C) 2019 Schirrms
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
 Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
+	'Class:PatchPanel' => 'Patch Panel',
+	'Class:PatchPanel/Attribute:patchpanelport_list' => 'Ports on this panel',
+	'Class:PatchCable' => 'Patch Cable',
+	'Class:PatchCable/Attribute:patchcablecolor_id' => 'Main Patch Cable Color',
+	'Class:PatchCable/Attribute:physcommtype_a_id' => 'Connector type on side A',
+	'Class:PatchCable/Attribute:physcommtype_a_name' => 'Connector type on side A name',
+	'Class:PatchCable/Attribute:remote_a_id' => '\'A\' side connected to',
+	'Class:PatchCable/Attribute:remote_a_name' => '\'A\' side connected to name',
+	'Class:PatchCable/Attribute:physcommtype_b_id' => 'Connector type on side B',
+	'Class:PatchCable/Attribute:physcommtype_b_name' => 'Connector type on side B name',
+	'Class:PatchCable/Attribute:remote_b_id' => '\'B\' side connected to',
+	'Class:PatchCable/Attribute:remote_b_name' => '\'B\' side connected to name',
+	'Class:PatchCable/Attribute:length' => 'Cable length',
+	'Class:PatchCable/Attribute:straight-crossed' => 'Straight or Crossed Cable',
+	'Class:PatchCable/Attribute:straight-crossed/Value:straight' => 'Straight',
+	'Class:PatchCable/Attribute:straight-crossed/Value:crossed' => 'Crossed',
+	'Class:PatchPanelPort' => 'Patch Panel Port',
+	'Class:PatchPanelPort/Attribute:connectableci_id' => 'Patch Panel this port belongs',
+	'Class:PatchPanelPort/Attribute:connectableci_name' => 'Name of Patch Panel this port belongs',
+	'Class:PatchPanelPort/Attribute:remotebackpanelport_id' => 'Connected to Remote Patch Panel Port',
+	'Class:PatchPanelPort/Attribute:remotebackpanelport_name' => 'Connected to Remote Patch Panel Port Name',
+	'Class:PatchPanelPort/Attribute:physcommtype_id' => 'Connector type',
+	'Class:PatchPanelPort/Attribute:physcommtype_name' => 'Connector Type Name',
 ));
 ?>
