903bbc7902fe6238fdf0d186884822069ad95afe
Schirrms <schirrms@schirrms.net>
2019-12-13 21:24:33 +0100

    Translation improvment (0.2.1.4)

--- schirrms-rack-cabling/en.dict.schirrms-rack-cabling.php
+++ schirrms-rack-cabling/en.dict.schirrms-rack-cabling.php
@@ -8,6 +8,8 @@
 
 Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
+	'Class:GenericCommLayer1Interface/Attribute:cable_id' => 'Cable Label',
+	'Class:GenericCommLayer1Interface/Attribute:cable_name' => 'Cable Label Name',
 	'Class:PatchPanel' => 'Patch Panel',
 	'Class:PatchPanel/Attribute:patchpanelport_list' => 'Ports on this panel',
 	'Class:PatchCable' => 'Patch Cable',
@@ -15,10 +17,12 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:PatchCable/Attribute:physcommtype_a_id' => 'Connector type on side A',
 	'Class:PatchCable/Attribute:physcommtype_a_name' => 'Connector type on side A name',
 	'Class:PatchCable/Attribute:remote_a_id' => '\'A\' side connected to',
+	'Class:PatchCable/Attribute:remote_a_id_finalclass_recall' => 'Equipment type on \'A\' side',
 	'Class:PatchCable/Attribute:remote_a_name' => '\'A\' side connected to name',
 	'Class:PatchCable/Attribute:physcommtype_b_id' => 'Connector type on side B',
 	'Class:PatchCable/Attribute:physcommtype_b_name' => 'Connector type on side B name',
 	'Class:PatchCable/Attribute:remote_b_id' => '\'B\' side connected to',
+	'Class:PatchCable/Attribute:remote_b_id_finalclass_recall' => 'Equipment type on \'B\' side',
 	'Class:PatchCable/Attribute:remote_b_name' => '\'B\' side connected to name',
 	'Class:PatchCable/Attribute:length' => 'Cable length',
 	'Class:PatchCable/Attribute:straight-crossed' => 'Straight or Crossed Cable',
---
+++ schirrms-rack-cabling/fr.dict.schirrms-rack-cabling.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2019 Schirrms
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class:GenericCommLayer1Interface/Attribute:cable_id' => 'Etiquette du câble',
+	'Class:GenericCommLayer1Interface/Attribute:cable_name' => 'Texte de l\'étiquette du câble',
+	'Class:PatchPanel' => 'Panneau de brassage',
+	'Class:PatchPanel/Attribute:patchpanelport_list' => 'Ports sur ce panneau',
+	'Class:PatchCable' => 'Jarretière',
+	'Class:PatchCable/Attribute:patchcablecolor_id' => 'Couleur principale de la jarretière',
+	'Class:PatchCable/Attribute:physcommtype_a_id' => 'Type de connecteur côté A',
+	'Class:PatchCable/Attribute:physcommtype_a_name' => 'Nom du type de connecteur côté A',
+	'Class:PatchCable/Attribute:remote_a_id' => 'Côté \'A\' connecté à',
+	'Class:PatchCable/Attribute:remote_a_id_finalclass_recall' => 'Type d\'équipement côté \'A\'',
+	'Class:PatchCable/Attribute:remote_a_name' => 'Nom de l\'équipement côté \'A\'',
+	'Class:PatchCable/Attribute:physcommtype_b_id' => 'Type de connecteur côté B',
+	'Class:PatchCable/Attribute:physcommtype_b_name' => 'Nom du type de connecteur côté B',
+	'Class:PatchCable/Attribute:remote_b_id' => 'Côté \'B\' connecté à',
+	'Class:PatchCable/Attribute:remote_b_id_finalclass_recall' => 'Type d\'équipement côté \'B\'',
+	'Class:PatchCable/Attribute:remote_b_name' => 'Nom de l\'équipement côté \'B\'',
+	'Class:PatchCable/Attribute:length' => 'Longueur de la jarretière',
+	'Class:PatchCable/Attribute:straight-crossed' => 'Câble droit ou croisé',
+	'Class:PatchCable/Attribute:straight-crossed/Value:straight' => 'Droit',
+	'Class:PatchCable/Attribute:straight-crossed/Value:crossed' => 'Croisé',
+	'Class:PatchPanelPort' => 'Port du panneau de brassage',
+	'Class:PatchPanelPort/Attribute:connectableci_id' => 'Panneau de brassage de ce port',
+	'Class:PatchPanelPort/Attribute:connectableci_name' => 'Nom du panneau de brassage de ce port',
+	'Class:PatchPanelPort/Attribute:remotebackpanelport_id' => 'Port du panneau de brassage distant',
+	'Class:PatchPanelPort/Attribute:remotebackpanelport_name' => 'Nom d port du panneau de brassage distant',
+	'Class:PatchPanelPort/Attribute:physcommtype_id' => 'Type de connecteur',
+	'Class:PatchPanelPort/Attribute:physcommtype_name' => 'Nom du type de connecteur',
+));
+?>
